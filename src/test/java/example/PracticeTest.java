c8b6ff99e972448bb89abbda2246e58c58cd5f39
JatinKumar9348 <jatin.papu.3@gmail.com>
2022-07-25 12:16:47 +0530

    2nd

--- src/test/java/example/PracticeTest.java
+++ src/test/java/example/PracticeTest.java
@@ -3,5 +3,7 @@ package example;
 public class PracticeTest {
 public static void main(String[] args) {
 	System.out.println("hello all");
+	System.out.println("Rakesh rakhyas");
+	System.out.println("bande maataram");
 }
 }
